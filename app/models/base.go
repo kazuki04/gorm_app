618d61fa2b9f668b2e75cc41340b9ffd0b727de4
kazuki04 <kazukik0402@gmail.com>
2021-09-15 21:19:42 +0900

    add migration for Article

--- app/models/base.go
+++ app/models/base.go
@@ -1,5 +1,15 @@
 package models
 
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
 func init() {
+	db, err := gorm.Open(sqlite.Open("gorm_app.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Article{})
 
 }
