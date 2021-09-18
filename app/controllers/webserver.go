dc2092e06cb1284236a23d4c59cf23baa582dc2f
kazuki04 <kazukik0402@gmail.com>
2021-09-18 13:57:26 +0900

    rename the view.html to index.html

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -10,36 +10,22 @@ import (
 	"strconv"
 )
 
-var templates = template.Must(template.ParseFiles("app/views/edit.html", "app/views/view.html", "app/views/new.html", "app/views/show.html"))
+var templates = template.Must(template.ParseFiles("app/views/edit.html", "app/views/index.html", "app/views/new.html", "app/views/show.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view|show)/([a-zA-Z0-9]+)$")
 
-func renderTemplates() {
-
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	templates.ExecuteTemplate(w, "view.html", nil)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", nil)
 }
 
 func newHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "new.html", nil)
 }
 
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-		}
-		fn(w, r, m[2])
-	}
-}
-
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	body := r.FormValue("body")
 	article := &models.Article{Title: r.FormValue("title"), Body: []byte(body)}
 	article.Create()
-	templates.ExecuteTemplate(w, "view.html", nil)
+	templates.ExecuteTemplate(w, "index.html", nil)
 }
 
 func showHandler(w http.ResponseWriter, r *http.Request) {
@@ -50,7 +36,7 @@ func showHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func StartWebServer() error {
-	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/new/", newHandler)
 	http.HandleFunc("/save/", saveHandler)
 	http.HandleFunc("/show/", showHandler)
