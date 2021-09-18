76556e0cdd1d3bca849b9ceed37f8cb41bcedc7c
kazuki04 <kazukik0402@gmail.com>
2021-09-18 13:41:22 +0900

    modify FindArticle func

--- app/models/article.go
+++ app/models/article.go
@@ -17,3 +17,9 @@ func (article *Article) Create() {
 func (article *Article) Update() {
 	db.Model(&article).Updates(Article{Title: article.Title, Body: article.Body})
 }
+
+func FindArticle(id int) Article {
+	var article Article
+	db.Table("articles").First(&article, id).Scan(&article)
+	return article
+}
